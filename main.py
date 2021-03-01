cc50b62c22aa9980ff18dcc372a51141f56ccd37
malawii <73660245+malawii@users.noreply.github.com>
2021-03-01 13:05:22 +0100

    V0.0.0.2
    Adding labyrinth's background and an objectif

--- main.py
+++ main.py
@@ -2,22 +2,126 @@
 import pygame
 from pygame.locals import *
 import random
-
-class Cellule:
-    """
-    définition d'une cellule
-    """
-    
-    def __init__(self,x, y):
-        """
-        créer une cellule positionnée en (x=colonne, y=ligne)
-        """        
-        self.x = x
-        self.y = y
-        #les murs sont dans l'ordre : S, E. 
-        #la valeur est à True si un mur est présent, False sinon
-        self.murs = {'N': True, 'O': True}
+class NoeudCellule() :
+    def __init__(self, pos, murs):
+        self.pos=pos
+        self.gauche=None
+        self.droit=None
+        self.murs=murs
         
+    def __repr__(self):
+        """permet d'afficher un arbre sous forme d'une liste"""
+        if self==None:
+            return None
+        else :
+            return str([self.pos, self.gauche, self.droit, self.murs])
+        
+    def creerfd(self, pos, murs):
+        '''crée et retourne un noeud fils droit ayant pour valeur le paramètre cle'''
+        assert self.droit is None,"Le Noeud possède déjà un fils droit"
+        self.droit=NoeudCellule(pos, murs)
+        return self.droit
+        
+    def creerfg(self, pos, murs):
+        '''crée et retourne un noeud fils gauche ayant pour valeur le paramètre cle'''
+        assert self.gauche is None,"Le Noeud possède déjà un fils gauche"
+        self.gauche=NoeudCellule(pos, murs)
+        return self.gauche
+
+'''algorithme récursif, construit un arbre aléatoirement à partir d'un noeud et dictionnaire de coordonnées
+   chaque noeud possède un attribut position, et murs, qui varie selon le chemin emprunté
+   par défaut, le fils gauche correspond aux noeuds qui évoluent sur l'axe des abcisses
+   le fils droit aux noeud qui évoluent sur l'axe des ordonnées'''
+def Recurs(noeud, dic):
+    posx, posy=noeud.pos[0], noeud.pos[1]
+    #si le noeud n'a pas été parcouru, le supprime du dictionnaire
+    if noeud.pos in dic:
+        del dic[noeud.pos]
+    chx=random.choice([1, 2])
+    #si le choix=1, créer une cellule sur l'axe x, puis l'axe y
+    if chx==1:
+        chx2=random.choice([1, 2])
+        #si 2ème choix=1, recule d'une cellule sur l'axe des abcisses
+        if chx2==1:
+            if (posx-1, posy) in dic.values() and noeud.gauche==None:
+                noeud.murs["O"]=False
+                Recurs(noeud.creerfg((posx-1, posy), {'N' : True, 'O' : True}), dic)
+            #si le fils gauche est occupé, vérifie le fils droit
+            if (posx-1, posy) in dic.values() and noeud.droit==None:
+                #si le fils droit est libre, y assigne une position
+                noeud.murs["O"]=False
+                Recurs(noeud.creerfd((posx-1, posy), {'N' : True, 'O' : True}), dic)
+        if (posx+1, posy) in dic.values() and noeud.gauche==None:
+            Recurs(noeud.creerfg((posx+1, posy), {'N' : True, 'O' : False}), dic)
+        #au cas où une incrémentation serait impossible sur le fils gauche, décrémenter
+        if (posx-1, posy) in dic.values() and noeud.gauche==None:
+            noeud.murs["O"]=False
+            Recurs(noeud.creerfg((posx-1, posy), {'N' : True, 'O' : True}), dic)
+        #si incrémentation et décrémentation impossible , réessayer sur le fils droit
+        if (posx+1, posy) in dic.values() and noeud.droit==None:
+            Recurs(noeud.creerfd((posx+1, posy), {'N' : True, 'O' : False}), dic)
+        if (posx-1, posy) in dic.values() and noeud.droit==None:
+            noeud.murs["O"]=False
+            Recurs(noeud.creerfd((posx-1, posy), {'N' : True, 'O' : True}), dic)
+        #même procédé mais pour l'axe des ordonnées
+        chx2=random.choice([1, 2])
+        if chx2==1:
+            if (posx, posy-1) in dic.values() and noeud.droit==None:
+                noeud.murs["N"]=False
+                Recurs(noeud.creerfd((posx, posy-1), {'N' : True, 'O' : True}), dic)
+            if (posx, posy-1) in dic.values() and noeud.gauche==None:
+                noeud.murs["N"]=False
+                Recurs(noeud.creerfg((posx, posy-1), {'N' : True, 'O' : True}), dic)
+        if (posx, posy+1) in dic.values() and noeud.droit==None:
+            Recurs(noeud.creerfd((posx, posy+1), {'N' : False, 'O' : True}), dic)
+        if (posx, posy-1) in dic.values() and noeud.droit==None:
+            noeud.murs["N"]=False
+            Recurs(noeud.creerfd((posx, posy-1), {'N' : True, 'O' : True}), dic)
+        if (posx, posy+1) in dic.values() and noeud.gauche==None:
+            Recurs(noeud.creerfg((posx, posy+1), {'N' : False, 'O' : True}), dic)
+        if (posx, posy-1) in dic.values() and noeud.gauche==None:
+            noeud.murs["N"]=False
+            Recurs(noeud.creerfg((posx, posy-1), {'N' : True, 'O' : True}), dic)
+    #si le choix=2, créer une cellule sur l'axe y, puis l'axe x, même procédé que si chx==1
+    else :
+        chx2=random.choice([1, 2])
+        if chx2==1:
+            if (posx, posy-1) in dic.values() and noeud.droit==None:
+                noeud.murs["N"]=False
+                Recurs(noeud.creerfd((posx, posy-1), {'N' : True, 'O' : True}), dic)
+            if (posx, posy-1) in dic.values() and noeud.gauche==None:
+                noeud.murs["N"]=False
+                Recurs(noeud.creerfg((posx, posy-1), {'N' : True, 'O' : True}), dic)
+        if (posx, posy+1) in dic.values() and noeud.droit==None:
+            Recurs(noeud.creerfd((posx, posy+1), {'N' : False, 'O' : True}), dic)
+        if (posx, posy-1) in dic.values() and noeud.droit==None:
+            noeud.murs["N"]=False
+            Recurs(noeud.creerfd((posx, posy-1), {'N' : True, 'O' : True}), dic)
+        if (posx, posy+1) in dic.values() and noeud.gauche==None:
+            Recurs(noeud.creerfg((posx, posy+1), {'N' : False, 'O' : True}), dic)
+        if (posx, posy-1) in dic.values() and noeud.gauche==None:
+            noeud.murs["N"]=False
+            Recurs(noeud.creerfg((posx, posy-1), {'N' : True, 'O' : True}), dic)
+            
+        chx2=random.choice([1, 2])
+        if chx2==1:
+            if (posx-1, posy) in dic.values() and noeud.gauche==None:
+                noeud.murs["O"]=False
+                Recurs(noeud.creerfg((posx-1, posy), {'N' : True, 'O' : True}), dic)
+            if (posx-1, posy) in dic.values() and noeud.droit==None:
+                noeud.murs["O"]=False
+                Recurs(noeud.creerfd((posx-1, posy), {'N' : True, 'O' : True}), dic)
+        if (posx+1, posy) in dic.values() and noeud.gauche==None:
+            Recurs(noeud.creerfg((posx+1, posy), {'N' : True, 'O' : False}), dic)
+        if (posx-1, posy) in dic.values() and noeud.gauche==None:
+            noeud.murs["O"]=False
+            Recurs(noeud.creerfg((posx-1, posy), {'N' : True, 'O' : True}), dic)
+        if (posx+1, posy) in dic.values() and noeud.droit==None:
+            Recurs(noeud.creerfd((posx+1, posy), {'N' : True, 'O' : False}), dic)
+        if (posx-1, posy) in dic.values() and noeud.droit==None:
+            noeud.murs["O"]=False
+            Recurs(noeud.creerfd((posx-1, posy), {'N' : True, 'O' : True}), dic)        
+
 ########      
 class Grille :
     """
@@ -29,32 +133,28 @@ class Grille :
         """
         self.nx = nx
         self.ny = ny
-        self.grille = []
-        for x in range(nx):
-            GrilleCol=[] #Création d'une liste correspondant à une colonne de la grille
-            for y in range(ny):
-                GrilleCol.append(Cellule(x,y))#Création et ajout des cellules dans la colonne
-            self.grille.append(GrilleCol)#Ajout de la colonne à la liste correspondant à la grille
-        #On applique l'algorithme de l'arbre binaire pour transformer la grille vierge en labyrinthe
+        CoorDico={}
         for x in range(nx): 
             for y in range(ny):
-                if (x,y)!=(0,0):
-                    if x==0:
-                        self.grille[x][y].murs["N"]=False
-                    elif y==0:
-                        self.grille[x][y].murs["O"]=False
-                    else:
-                        mur=random.choice(["O","N"])
-                        self.grille[x][y].murs[mur]=False
+                CoorDico[(x, y)]=(x, y)
+        item=NoeudCellule(CoorDico[(0, 0)], {'N' : True, 'O' : True})
+        Recurs(item, CoorDico)
+        self.grille = item
                         
+    def GetMurs(self, x, y, arbre):
         
-    def cellule(self, x, y):
-        """
-        retourne la cellule (objet de classe Cellule) de la grille de position (x=colonne, y=ligne)
-        """
-        return self.grille[x][y]
-    
-       
+        coordo=(x, y)
+        
+        def parcours(coord, arbre):
+            if arbre.pos==coord:
+                global resu
+                resu=arbre.murs
+            if arbre.gauche!=None:
+                parcours(coord, arbre.gauche)
+            if arbre.droit!=None:
+                parcours(coord, arbre.droit)
+        parcours(coordo, arbre)
+        return resu
     def __str__(self):
         """
         retourne une chaine représentant le labyrinthe. Permet de visualiser la grille à l'aide de la fonction print.
@@ -87,7 +187,7 @@ class Personnage():
     d'un personnage tel que sa positon ou son inventaire"""
     def __init__(self):
         """Création d'un personnage avec sa position et son inventaire"""
-        self.inventaire={"Torche":False}
+        self.inventaire={"Torche":False, "Tresor":False}
         self.pos=[0,0]
     
     def gauche(self):
@@ -101,22 +201,27 @@ class Personnage():
 
     def bas(self):
         self.pos[1]+=1
-Link=Personnage()
-print(Link.pos)
-Link.gauche()
-Link.gauche()
-print(Link.pos)
 
-import pygame
-from pygame.locals import *
+class Objet():
+    def __init__(self, nom, pos):
+        self.nom=nom
+        self.pos=pos
+def VerifObj(dico, pos, obj):
+    res=False
+    for item in dico:
+        if dico.get(obj)==pos:
+            res=True
+    return res
+	
 pygame.init()
 
 color = (200,200,200)
-size = (800,600)
+size = (1280,720)
 try:
-    Donjon = Grille(10,10)
-    print(Donjon)
+    x, y=39,22 
+    Donjon = Grille(x,y)
     Joueur=Personnage()
+    Tresor=Objet("Trésor", [x, y])
 
     fenetre=pygame.display.set_mode(size)#fenêtre de taille 640*480
     Labyrinth=pygame.Surface(size,pygame.SRCALPHA)
@@ -126,13 +231,11 @@ try:
 
     
     #Chargement des murs
-    #coin=pygame.image.load('Sprites/Walls/Corner.png').convert_alpha()
-    #haut=pygame.image.load('Sprites/Walls/Upper.png').convert_alpha()
-    #gauche=pygame.image.load('Sprites/Walls/Left.png').convert_alpha()
-    murs=[pygame.image.load('Sprites/Walls/Corner.png').convert_alpha(),
-          pygame.image.load('Sprites/Walls/Upper.png').convert_alpha(),
-          pygame.image.load('Sprites/Walls/Left.png').convert_alpha()]
-    #murs=[coin,haut,gauche]
+    coin=pygame.image.load('Sprites/Walls/Corner.png').convert_alpha()
+    haut=pygame.image.load('Sprites/Walls/Upper.png').convert_alpha()
+    gauche=pygame.image.load('Sprites/Walls/Left.png').convert_alpha()
+    vide=pygame.image.load('Sprites/Walls/None.png').convert_alpha()
+    sol=pygame.image.load('Sprites/Walls/Floor.png').convert_alpha()
     #Chargement des sprites du personnage
     PFront=pygame.image.load('Sprites/Char/PFront.png').convert_alpha()
     PFront=pygame.transform.scale(PFront,(24,24))
@@ -143,34 +246,50 @@ try:
     PRight=pygame.image.load('Sprites/Char/PRight.png').convert_alpha()
     PRight=pygame.transform.scale(PRight,(24,24))
     CharacterRect=pygame.Rect(7,7,24,24)
-    
+    TresorImg=pygame.image.load('Sprites/Items/diamant.png').convert_alpha()
     #Affichage du Donjon
     coord_x = 0
     coord_y = 0
     for i in range(Donjon.ny):
         for j in range(Donjon.nx):
-            if Donjon.cellule(j,i).murs["N"]==True & Donjon.cellule(j,i).murs["O"]==True:
-                Labyrinth.blit(murs[0],(coord_x,coord_y))
-            elif Donjon.cellule(j,i).murs["N"]==False :
-                Labyrinth.blit(murs[2],(coord_x,coord_y))
-            elif Donjon.cellule(j,i).murs["O"]==False:
-                Labyrinth.blit(murs[1],(coord_x,coord_y))
+            Labyrinth.blit(sol,(coord_x,coord_y))
+            if Donjon.GetMurs(j,i, Donjon.grille)["N"]==True and Donjon.GetMurs(j,i, Donjon.grille)["O"]==True:
+                Labyrinth.blit(coin,(coord_x,coord_y))
+            elif Donjon.GetMurs(j,i, Donjon.grille)["N"]==False and Donjon.GetMurs(j,i, Donjon.grille)["O"]==True :
+                Labyrinth.blit(gauche,(coord_x,coord_y))
+            elif Donjon.GetMurs(j,i, Donjon.grille)["O"]==False and Donjon.GetMurs(j,i, Donjon.grille)["N"]==True:
+                Labyrinth.blit(haut,(coord_x,coord_y))
+            elif Donjon.GetMurs(j,i, Donjon.grille)["O"]==False and Donjon.GetMurs(j,i, Donjon.grille)["N"]==False:
+                Labyrinth.blit(vide,(coord_x,coord_y))
             coord_x+=32
+            fenetre.blit(Labyrinth,(0,0))
+            pygame.display.flip()
+            pygame.time.delay(5)
         coord_x=0
         coord_y+=32
     #Affichage des bords droit du donjon
     coord_x=Donjon.nx*32
     for y in range(0,coord_y,32):
-        Labyrinth.blit(murs[2],(coord_x,y))
+        Labyrinth.blit(gauche,(coord_x,y))
     
     #Affichage des bords inférieur du donjon
     coord_y=Donjon.ny*32
     for x in range(0,coord_x,32):
-        Labyrinth.blit(murs[1],(x,coord_y))
+        Labyrinth.blit(haut,(x,coord_y))
     
+    #Définition de la position du trésor
+    x=random.randint(Tresor.pos[0]-(Tresor.pos[0]//4), Tresor.pos[0])
+    y=random.randint(Tresor.pos[1]-(Tresor.pos[1]//4), Tresor.pos[1])
+    Tresor.pos[0]=x-1
+    Tresor.pos[1]=y-1
+        
+    Objets={'Tresor' : Tresor.pos}
+        
+    Gameboard.fill((0,0,0,0))
     Gameboard.blit(PFront,CharacterRect)
-    fenetre.blit(Gameboard,(0,0))
+    Gameboard.blit(TresorImg,((x-1)*32,(y-1)*32))
     fenetre.blit(Labyrinth,(0,0))
+    fenetre.blit(Gameboard,(0,0))
     pygame.display.flip()
 
     continuer=True
@@ -184,48 +303,49 @@ try:
             if event.type==KEYDOWN:
                 if event.key==K_LEFT:
                     if Joueur.pos[0]!=0:
-                        if Donjon.cellule(Joueur.pos[0],Joueur.pos[1]).murs["O"]==False:
-                            Gameboard.fill((0,0,0))
+                        if Donjon.GetMurs(Joueur.pos[0],Joueur.pos[1], Donjon.grille)["O"]==False:
+                            
+                            Gameboard.fill((0,0,0,0))
                             CharacterRect=CharacterRect.move(-32,0)
                             Gameboard.blit(PLeft,CharacterRect)
-                            fenetre.blit(Gameboard,(0,0))
-                            fenetre.blit(Labyrinth,(0,0))
                             Joueur.gauche()
 
                 if event.key==K_RIGHT:
                     
-                    if Joueur.pos[0]+1<Donjon.nx:
-                        if Donjon.cellule(Joueur.pos[0]+1,Joueur.pos[1]).murs["O"]==False:
+                    if Joueur.pos[0]+1<Donjon.nx :
+                        if Donjon.GetMurs(Joueur.pos[0]+1,Joueur.pos[1], Donjon.grille)["O"]==False:
                             
-                            Gameboard.fill((0,0,0))
+                            Gameboard.fill((0,0,0,0))
                             CharacterRect=CharacterRect.move(32,0)
                             Gameboard.blit(PRight,CharacterRect)
-                            fenetre.blit(Gameboard,(0,0))
-                            fenetre.blit(Labyrinth,(0,0))
                             Joueur.droite()
                             
                 if event.key==K_UP:
                     if Joueur.pos[1]!=0:
-                        if Donjon.cellule(Joueur.pos[0],Joueur.pos[1]).murs['N']==False:
-                        
-                            Gameboard.fill((0,0,0))
+                        if Donjon.GetMurs(Joueur.pos[0],Joueur.pos[1], Donjon.grille)['N']==False:
+                            
+                            Gameboard.fill((0,0,0,0))
                             CharacterRect=CharacterRect.move(0,-32)
                             Gameboard.blit(PBack,CharacterRect)
-                            fenetre.blit(Gameboard,(0,0))
-                            fenetre.blit(Labyrinth,(0,0))
                             Joueur.haut()
             
                 if event.key==K_DOWN:
                     if Joueur.pos[1]+1<Donjon.ny:
-                        if Donjon.cellule(Joueur.pos[0],Joueur.pos[1]+1).murs['N']==False:
-
-                            Gameboard.fill((0,0,0))
+                        if Donjon.GetMurs(Joueur.pos[0],Joueur.pos[1]+1, Donjon.grille)['N']==False:
+                            
+                            Gameboard.fill((0,0,0,0))
                             CharacterRect=CharacterRect.move(0,32)
                             Gameboard.blit(PFront,CharacterRect)
-                            fenetre.blit(Gameboard,(0,0))
-                            fenetre.blit(Labyrinth,(0,0))
                             Joueur.bas()
                             
+                if VerifObj(Objets, Joueur.pos, "Tresor")==False and Joueur.inventaire["Tresor"]==False:
+                    Gameboard.blit(TresorImg,((x-1)*32,(y-1)*32))
+                else:
+                    Joueur.inventaire["Tresor"]=True
+                    continuer=False
+                
+                fenetre.blit(Labyrinth,(0,0))
+                fenetre.blit(Gameboard,(0,0))
             pygame.display.flip()
                 
 finally:
